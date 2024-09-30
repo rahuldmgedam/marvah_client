64acfb5acd558b1b322e8061e1630f2226e3633d
rahuldmgedam <gedamrahul19@gmail.com>
2024-09-30 11:16:06 +0530

    host rahul 30 sept

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='p-2 bg-black text-white font-semibold text-center'>Designed & Developed by Royals Webtech Pvt. Ltd. ©️2024</div>
-  )
-}
+    <div className="p-2 bg-black text-white font-semibold text-center">
+      {/* Designed & Developed by Royals Webtech Pvt. Ltd. ©️2024 */}
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
